7511ebaed774cedd71062fbe223d79b767d32af3
Eduardo Pilati <pilatieduardo@gmail.com>
2023-12-22 17:33:25 -0300

    adjust https over nginx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 use App\Classes\PaginationUrlWindow;
 use Illuminate\Pagination\UrlWindow;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $aliasLoader = AliasLoader::getInstance();
         $aliasLoader->alias(UrlWindow::class, PaginationUrlWindow::class);
+
+        if (Str::contains(Config::get('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**

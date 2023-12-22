51e0a57526e61710810fca590aa8f1664002dfda
Eduardo Pilati <pilatieduardo@gmail.com>
2023-12-22 17:49:00 -0300

    testing fix

--- docker_data/mysql_entrypoint/create_drivit_database.sql
+++ docker_data/mysql_entrypoint/create_drivit_database.sql
@@ -1,3 +1,6 @@
 create user drivit identified by "drivit";
 create database drivit;
 grant all privileges on drivit.* to drivit;
+
+create database testing;
+grant all privileges on testing.* to drivit;
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 php artisan cache:clear
 php artisan route:clear
-php artisan config:cache
-php artisan view:cache
+php artisan config:clear
+php artisan view:clear
 php artisan migrate --force --isolated
 php artisan serve --host 0.0.0.0
